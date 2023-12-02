17a96f504ba27b26520ee84e02703d2e60056536
Domen Soklic <soklicd@gmail.com>
2023-12-02 07:31:56 +0100

    Imporved utils

--- solutions/utils.py
+++ solutions/utils.py
@@ -1,3 +1,8 @@
+import re
+
 def readFile(file_path):
     with open(file_path, 'r') as f:
         return [line.rstrip() for line in f.readlines()]
+    
+def extractNumber(input):
+    return re.search(r"(\d+)", input).group(1)
